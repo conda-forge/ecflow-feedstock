1452b8d97562f9de7f6cbb1e9f5c9d93edf96eb1
Iain Russell <40060766+iainrussell@users.noreply.github.com>
2024-02-13 14:11:59 +0000

    Added D_LIBCPP_DISABLE_AVAILABILITY for maxos in build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -22,6 +22,9 @@ if [[ $(uname) == Darwin && ${target_platform} == osx-64 ]]; then
     # Disable use of std::aligned_alloc by boost, as this is not available on macOS 10.9
     export CXXFLAGS="$CXXFLAGS -DBOOST_ASIO_DISABLE_STD_ALIGNED_ALLOC"
 
+    # https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+    export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+
     # Disable ecflow_http build, as it uses C++17 features only available on macOS 10.12+
     ENABLE_HTTP=OFF
     # Disable ecflow_udp build, as it uses C++17 features only available on macOS 10.13+
