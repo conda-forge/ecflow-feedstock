8139686b17c085dd2a16c5c1e267f7df133b07b7
marcosbento <marcosbento@gmail.com>
2025-05-06 16:26:15 +0100

    Correct Cereal include dirs

--- recipe/build.sh
+++ recipe/build.sh
@@ -64,7 +64,7 @@ cmake ${CMAKE_ARGS} \
       -D Python3_FIND_STRATEGY=LOCATION \
       -D Python3_EXECUTABLE=$PYTHON \
       -D ENABLE_PYTHON_UNDEF_LOOKUP=$UNDEF_LOOKUP \
-      -D CEREAL_INCLUDE_DIRS=../3rdparty/cereal/include \
+      -D Cereal_INCLUDE_DIRS=../3rdparty/cereal/include \
       -D JSON_INCLUDE_DIRS=../3rdparty/json/include \
       -D HTTPLIB_INCLUDE_DIRS=../3rdparty/cpp-httplib/include \
       ..
